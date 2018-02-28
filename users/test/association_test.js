ce4a987c21acb33778dbaa931c719a3eb00cd080
schirag1993 <pears.chirag@gmail.com>
2018-02-28 21:17:10 +0530

    add association test

--- users/test/association_test.js
+++ users/test/association_test.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const assert = require('assert');
 
 const User = require('../src/user');
 const Comment = require('../src/comment');
@@ -25,14 +26,49 @@ describe('Associations', () => {
       .then(() => done());
   });
 
-  it.only('saves a relation between a user and a blog post', (done) => {
+  it('saves a full relation graph', (done) => {
+    User.findOne({name: 'joe'})
+      .populate({
+        path: 'blogPosts',
+        populate: {
+          path: 'comments',
+          model: 'comment',
+          populate: {
+            path: 'user',
+            model: 'user'
+          }
+        }
+      })
+      .then((user) => {
+        assert(user.name === 'joe');
+        assert(user.blogPosts[0].title === 'JS is good');
+        assert(user.blogPosts[0].comments[0].content === 'Congrats on new post');
+        assert(user.blogPosts[0].comments[0].user.name === 'joe');
+        done();
+      });
+  });
+
+  it('saves a relation between a user and a blog post', (done) => {
     User.findOne({name: 'joe'})
       .populate('blogPosts')
       .then((user) => {
-        console.log(user);
+        assert(user.blogPosts[0].title === 'JS is good')
         done();
-      })
-  })
+      });
 
-
+    // it('saves a full relation graph', (done) => {
+    //   User.findOne({name: 'joe'})
+    //     .populate({
+    //       path: 'blogPosts',
+    //       populate: {
+    //         path: 'comments',
+    //         model: 'comment'
+    //       }
+    //     })
+    //     .then((user) => {
+    //       console.log(user);
+    //       done();
+    //     });
+    // });
+  });
 });
